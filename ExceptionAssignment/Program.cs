0244fca7501f9c029b91a7634e2920e1b9544ac6
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2020-12-28 21:42:07 -0700

    Add project files.

---
+++ ExceptionAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> newNum = new List<int> { 33, 44, 50, 100 };
+            Console.WriteLine("Enter a number to divide by.");
+            int numResult = Convert.ToInt32(Console.ReadLine());
+
+            foreach(int num in newNum)
+            {
+                int divNum = newNum / numResult;
+
+            }
+        }
+    }
+}

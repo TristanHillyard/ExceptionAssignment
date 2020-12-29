16db7ac85e651a50d472a55d564967735b31a48a
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2020-12-29 08:39:15 -0700

    finishing try/catch blocks

--- ExceptionAssignment/Program.cs
+++ ExceptionAssignment/Program.cs
@@ -9,13 +9,26 @@ namespace ExceptionAssignment
         {
             List<int> newNum = new List<int> { 33, 44, 50, 100 };
             Console.WriteLine("Enter a number to divide by.");
-            int numResult = Convert.ToInt32(Console.ReadLine());
-
-            foreach(int num in newNum)
+            
+            try
             {
-                int divNum = newNum / numResult;
-
+                int numResult = Convert.ToInt32(Console.ReadLine());
+                foreach (int num in newNum)
+                {
+                    int divNum = num / numResult;
+                    Console.WriteLine(divNum);
+                    Console.ReadLine();
+                }
             }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Unable to divide these numbers by Zero.");
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Cannot divide letter format.");
+            }
+            Console.WriteLine("The program has finished the try/catch block.");
         }
     }
 }
